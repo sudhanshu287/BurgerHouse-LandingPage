ed31e39743fe17dd4a665f3268acd2bbc089e440
Sudhanshu Patel <patelsudhanshu290@gmail.com>
2023-02-21 22:38:15 +0530

    Hamburger menu fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../assets/burger-logo.png';
 import  './Nav.css';
 
 const Nav = () => {
+  const [openNavMenu,setOpenNavMenu]=useState(false);
   return (
     <nav className='navbar'>
         <div className="logo">
             <img src={Logo } alt="logo" />
         </div>
-        <div className="navContainer">
+        <div className={openNavMenu?"navContainer open":"navContainer"}>
             <div className="deliveryNumber">
             <div><i class="ri-e-bike-2-fill"></i></div>
             <div>Express Delivery +1234567890</div>
@@ -22,7 +23,7 @@ const Nav = () => {
             </ul>
             
         </div>
-        <div className="bx bx-menu" id="menu-icon"><i class="ri-menu-3-line"></i></div>
+        <div className="bx bx-menu" id="menu-icon" onClick={()=>setOpenNavMenu(!openNavMenu)}><i class={!openNavMenu?"ri-menu-3-line":"ri-close-line"}></i></div>
     </nav>
   );
 };
